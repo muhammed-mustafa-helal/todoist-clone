8e33d71a8668e9dcdf6a9fb60e8c54bd03ec2c55
Muhammed Mustafa <muhammed.muhammed.ext@siemens.com>
2022-02-22 14:01:07 +0100

    added project and selected project context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Content from "./components/layout/Content";
 import Header from "./components/layout/Header";
+import { ProjectsProvider, SelectedProjectProvider } from "./context";
 
 export const App = () => (
-  <div>
-    <Header />
-    <Content />
-  </div>
+  <SelectedProjectProvider>
+    <ProjectsProvider>
+      <div>
+        <Header />
+        <Content />
+      </div>
+    </ProjectsProvider>
+  </SelectedProjectProvider>
 );
---
+++ src/context/selected-project-context.js
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from "react";
+
+export const SelectedProjectContext = createContext();
+export const SelectedProjectProvider = ({ children }) => {
+  const { selectedProject, setSelectedProject } = useState("");
+
+  return (
+    <SelectedProjectContext.Provider value={{ selectedProject, setSelectedProject }}>
+      {children}
+    </SelectedProjectContext.Provider>
+  );
+};
+
+export const useSelectedProjectValue = () =>
+  useContext(SelectedProjectContext);
