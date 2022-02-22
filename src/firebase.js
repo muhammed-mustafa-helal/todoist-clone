7d937a69f95c8e57e4a42c06132cdd9acb5736fe
Muhammed Mustafa <muhammed.muhammed.ext@siemens.com>
2022-02-22 11:53:12 +0100

    Connected the app to firebase and created two custom hooks to fetch tasks and projects

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9rTITN0IS-RGlbLvlZ57KRmqboy5WtKg",
+  authDomain: "todoist-muhammed.firebaseapp.com",
+  databaseURL:
+    "https://todoist-muhammed-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todoist-muhammed",
+  storageBucket: "todoist-muhammed.appspot.com",
+  messagingSenderId: "647747005989",
+  appId: "1:647747005989:web:91675c82f33ad0758099cc",
+};
+
+export { firebaseConfig as firebase };
